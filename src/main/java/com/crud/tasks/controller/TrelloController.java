76bb821358c19fd7455d78b021fcc8525ac433b0
daniellsoon <daniel.adamiec@hotmail.com>
2020-03-30 21:24:24 +0200

    TESTS added

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/v1/trello")
@@ -21,22 +21,16 @@ public class TrelloController {
     private TrelloClient trelloClient;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
-    public void getTrelloBoards() {
+    public List<TrelloBoardDto> getTrelloBoards() {
 
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
         trelloBoards
                 .stream()
                 .filter(e -> !e.getId().isEmpty() && !e.getName().isEmpty() && e.getName().contains("Kodilla"))
-                .forEach(trelloBoardDto -> {
-
-                    System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
-
-                    System.out.println("This board contains lists: ");
-
-                    trelloBoardDto.getLists().forEach(trelloList ->
-                            System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
-                });
+                .collect(Collectors.toList());
+//
+        return trelloBoards;
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
